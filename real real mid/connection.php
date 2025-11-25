e6b12e53b2e67b45af94958e825e76d1f69dd700
23070550-BuiVietPhuc <23070550@vnu.edu.vn>
2025-11-25 11:49:56 +0700

    setup auto deploy

--- real real mid/connection.php
+++ real real mid/connection.php
@@ -1,5 +1,29 @@
 <?php
-//creating a database connection - $con is a variable use for just connection class
-$con=mysqli_connect("localhost","root","") or die(mysqli_error($con));
-mysqli_select_db($con,"recipe_share_db") or die(mysqli_error($con));
+// Kiểm tra xem đang chạy ở đâu
+$whitelist = array('127.0.0.1', '::1', 'localhost');
 
+if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
+    // --- CẤU HÌNH CHO MÁY TÍNH CỦA MÀY (XAMPP) ---
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db_name = "recipe_share_db";
+} else {
+    // --- CẤU HÌNH CHO INFINITYFREE ---
+    // LƯU Ý: Mày phải vào web InfinityFree mục MySQL Databases để xem cái 'sqlXXX' chính xác là gì nhé (Ví dụ: sql305.infinityfree.com)
+    // Tao để tạm sql300, mày phải sửa lại cho đúng cái của mày
+    $host = "sql305.infinityfree.com"; 
+    $user = "if0_40503839";
+    $pass = "dungphuctai"; 
+    $db_name = "if0_40503839_recipe_share_db";
+}
+
+$con = mysqli_connect($host, $user, $pass, $db_name);
+
+if (mysqli_connect_errno()) {
+    die("Kết nối thất bại: " . mysqli_connect_error());
+}
+
+// Đảm bảo tiếng Việt không bị lỗi font
+mysqli_set_charset($con, 'utf8');
+?>
